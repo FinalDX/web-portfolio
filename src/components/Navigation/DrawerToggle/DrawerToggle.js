8292e162ce83ab92ca428f74137350938764ac09
Sean Fenner <fennersean@yahoo.com>
2020-08-12 12:09:30 -0700

    Style fix

--- src/components/Navigation/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/DrawerToggle/DrawerToggle.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { IoMdMenu } from 'react-icons/io';
+import React from "react";
+import { IoMdMenu } from "react-icons/io";
 
-import SideDrawer from './SideDrawer/SideDrawer';
-import Backdrop from '../../UI/Backdrop/Backdrop';
+import SideDrawer from "./SideDrawer/SideDrawer";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
-import classes from './DrawerToggle.module.css';
+import classes from "./DrawerToggle.module.css";
 
-const drawerToggle = props => {
-
-    return (
-        <div className={classes.DrawerToggle}>
-            <div>
-                <IoMdMenu size='2em' onClick={props.clicked} />
-            </div>
-            <SideDrawer show={props.show} clicked={props.clicked}/>
-            <Backdrop show={props.show} clicked={props.clicked}/>
-        </div>
-    );
-}
+const drawerToggle = (props) => {
+  return (
+    <div className={classes.DrawerToggle}>
+      <div>
+        <IoMdMenu
+          size="2em"
+          onClick={props.clicked}
+          style={{ cursor: "pointer" }}
+        />
+      </div>
+      <SideDrawer show={props.show} clicked={props.clicked} />
+      <Backdrop show={props.show} clicked={props.clicked} />
+    </div>
+  );
+};
 
 export default drawerToggle;
