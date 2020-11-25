45a6cc32a0e826ade68f38c7ca0a662c83062e02
Sean Fenner <fennersean@yahoo.com>
2020-11-25 12:39:16 -0800

    updated home links

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Home.module.css';
-import Button from '../UI/Button/Button';
-import NavigationItem from '../Navigation/NavigationItems/NavigationItem/NavigationItem';
+import classes from "./Home.module.css";
+import Button from "../UI/Button/Button";
+import NavigationItem from "../Navigation/NavigationItems/NavigationItem/NavigationItem";
 
-const home = props => (
-        <div className={classes.Home}>
-            <h1>Sean Fenner &mdash; Web Developer</h1>
-            <NavigationItem clicked={props.clicked} link="/Projects" >
-                <Button btnType="Full">Projects</Button>
-            </NavigationItem>
-            <NavigationItem clicked={props.clicked} link="/Contact" >
-                <Button btnType="Ghost">Contact</Button>
-            </NavigationItem>
-        </div>
+const home = (props) => (
+  <div className={classes.Home}>
+    <h1>Sean Fenner &mdash; Web Developer</h1>
+    <NavigationItem clicked={props.clicked} link="#Projects">
+      <Button btnType="Full">Projects</Button>
+    </NavigationItem>
+    <NavigationItem clicked={props.clicked} link="#Contact">
+      <Button btnType="Ghost">Contact</Button>
+    </NavigationItem>
+  </div>
 );
 
 export default home;
